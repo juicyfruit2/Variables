58caaaf4fadeda70be22ef0d511c457f6cd5dc93
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-01 15:02:49 +0200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+'''Input a string Variable
+Input a float point Variable
+Input a integer Variable
+Input a boolean Variable '''
+
+spam = 'joshua'
+print(spam)
+spam = 3.14
+print(spam)
+spam = 2
+print(spam)
+male=True
+print(male)
+
